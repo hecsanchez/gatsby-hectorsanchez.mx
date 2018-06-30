46373b7541352334750189fa04d6a05d9d449ce2
Hector Sanchez <hola@hectorsanchez.mx>
2018-06-30 01:16:51 -0500

    Adds markdown support

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import styles from './intro.module.css'
+import Markdown from 'react-markdown'
+import './intro.scss'
 
 export default ({ person }) => (
-  <div className={styles.hero}>
+  <div className="hero">
     <img
-      className={styles.heroImage}
+      className="heroImage"
       src={`${person.node.image.file.url}?w=1180&h=600&fit=pad&bg=rgb:000000`}
       alt=""
     />
-    <div className={styles.heroDetails}>
-      <h3 className={styles.heroHeadline}>{person.node.name}</h3>
-      <p className={styles.heroTitle}>{person.node.title}</p>
-      <p>{person.node.shortBio.shortBio}</p>
+    <div className="heroDetails">
+      <h3 className="heroHeadline">{person.node.name}</h3>
+      <p className="heroTitle">{person.node.title}</p>
+      <Markdown source={person.node.shortBio.shortBio}/>
     </div>
   </div>
 )
